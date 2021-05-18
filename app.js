06abef96ed3d440154563b879754f4dfe174b850
GillesSchneider <57995282+GillesSchneider@users.noreply.github.com>
2021-05-18 18:49:23 -0400

    EDIT: Add animations on map and quote

--- app.js
+++ app.js
@@ -8,16 +8,23 @@ sr.reveal('.name-websites', {
 });
 
 sr.reveal('.t-intro', {
+    delay: 500
 });
 
 sr.reveal('.p-intro', {
+     delay: 500
 });
 
 sr.reveal('.contact-me', {
+     delay: 500
 });
 
 sr.reveal('.skills', {
 })
+
+sr.reveal('.s-span', {
+}, 100)
+
 
 sr.reveal('.t-skills', {
 
@@ -60,4 +67,19 @@ sr.reveal('.t-tag-list', {
 
 sr.reveal('.t-paragraph', {
     duration: 750,
+}, 50)
+
+sr.reveal('#map', {
+    duration: 750,
+    origin: 'left',
+    distance:'50px',
+    reset:true
+}, 50)
+
+
+sr.reveal('.component', {
+    duration: 750,
+    origin: 'right',
+    distance:'50px',
+    reset:true
 }, 50)
