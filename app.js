01f2c4df606b3f5aa187f7e2dec850e72716b143
GillesSchneider <57995282+GillesSchneider@users.noreply.github.com>
2021-05-18 18:35:22 -0400

    EDIT: Add ScrollReveal, add basic animations

---
+++ app.js
@@ -0,0 +1,63 @@
+const sr = ScrollReveal({
+    duration: 1000,
+});
+sr.reveal('.profile-image', {
+});
+
+sr.reveal('.name-websites', {
+});
+
+sr.reveal('.t-intro', {
+});
+
+sr.reveal('.p-intro', {
+});
+
+sr.reveal('.contact-me', {
+});
+
+sr.reveal('.skills', {
+})
+
+sr.reveal('.t-skills', {
+
+}, 50)
+
+sr.reveal('.tag-list', {
+}, 50)
+
+sr.reveal('.li-timeline', {
+    origin: 'bottom',
+    distance: '300px',
+}, 50)
+
+sr.reveal('.t-tag-list', {
+    duration: 750,
+}, 50)
+
+
+sr.reveal('.t-date', {
+    duration: 750,
+    distance: '50px',
+}, 50)
+
+sr.reveal('.t-title-1', {
+    origin: 'right',
+    duration: 750,
+    distance: '50px',
+}, 50)
+
+sr.reveal('.t-title-2', {
+    origin: 'right',
+    duration: 750,
+    distance: '50px',
+}, 50)
+
+
+sr.reveal('.t-tag-list', {
+    duration: 750,
+}, 50)
+
+sr.reveal('.t-paragraph', {
+    duration: 750,
+}, 50)
